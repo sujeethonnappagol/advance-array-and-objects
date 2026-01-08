c366ebee86324b8a38cc799df00b096b2004d0d1
sujeet <sujeethonnappagol3@gmail.com>
2026-01-08 20:04:07 +0530

    advace concepts on array and objects

---
+++ app.js
@@ -0,0 +1,25 @@
+// let arr = [1, 2, 3, 4, 5];
+// arr.shift(); // removes the first element (1)
+// console.log(arr); 
+// arr.unshift(-2,-1,0); // adds 0 at the beginning
+// console.log(arr);
+// arr.splice(2, 3, 'a', 'b', 'c'); // at index 2, remove 0 elements, add 'a', 'b', 'c'
+// console.log(arr);
+// new_arr=arr.slice(2, 4); // creates a new array from index 2 to 5 (not including 5)
+// console.log(new_arr);
+
+// concat
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let result = arr1.concat(arr2);
+// console.log(result); // [1, 2, 3, 4, 5, 6]
+
+// // join
+// let arr = ['Fire', 'Air', 'Water'];
+// let joined = arr.join('-');
+// console.log(joined); // "Fire-Air-Water"
+
+
+let arr = ['sujeet' , 'kumar' , 'singh'];
+let joined = arr.join('-');
+console.log(joined); // "sujeet-kumar-singh"
